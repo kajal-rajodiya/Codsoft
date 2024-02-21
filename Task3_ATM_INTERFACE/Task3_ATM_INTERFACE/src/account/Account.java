7edc4f565121550e683d3fff4fb44d52abfc2a66
Kajal-rajodiya <kajalrajodiya1@gmail.com>
2024-02-22 00:52:52 +0530

    Add files via upload

---
+++ Task3_ATM_INTERFACE/Task3_ATM_INTERFACE/src/account/Account.java
@@ -0,0 +1,14 @@
+package account;
+
+public class Account {
+	double accountBalance;
+
+	public double getAccountBalance() {
+		return accountBalance;
+	}
+
+	public void setAccountBalance(double accountBalance) {
+		this.accountBalance = accountBalance;
+	}
+
+}
